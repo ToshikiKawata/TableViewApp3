17a9064effc33b94eb7638d97ef2499174b3129d
ToshikiKawata <kawatatohsiki@icloud.com>
2021-11-03 00:35:17 +0900

    Initial Commit

---
+++ TableViewApp3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewApp3
+//
+//  Created by 川田俊希 on 2021/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
